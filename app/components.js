a8f1f916815e943589d1913ded52ec487f9fa28a
Adam Johnston <adam.johnston3679@gmail.com>
2016-04-16 11:45:31 +0100

    fixture and component examples - this will need to be generated by a script later

---
+++ app/components.js
@@ -0,0 +1,8 @@
+import Button from '../example-components/Button'
+import Tabbable from '../example-components/Tabbable'
+
+const components = {
+  Button, Tabbable
+}
+
+export default components
---
+++ app/fixtures.js
@@ -0,0 +1,18 @@
+const fixtures = {
+  Button: [{
+    header: 'A basic button',
+    props: {
+      className: 'primary',
+      children: 'Button'
+    },
+  }],
+
+  Tabbable: [{
+    header: 'Tabbing',
+    props: {
+      className: 'basic'
+    }
+  }]
+}
+
+export default fixtures
