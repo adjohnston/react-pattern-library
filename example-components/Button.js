617818f1e824a33e59d2afbc13ed9e5adf7e6995
Adam Johnston <adam.johnston3679@gmail.com>
2016-04-16 11:37:10 +0100

    some very basic component examples

---
+++ example-components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <button
+      className={props.className}>
+      {props.children}
+    </button>
+  )
+}
+
+export default Button
---
+++ example-components/Tabbable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tabbable = () => {
+  return (
+    <span>I am a tab component</span>
+  )
+}
+
+export default Tabbable
