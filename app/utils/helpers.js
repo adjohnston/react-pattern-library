afdc2ad936401b3078fae6b87f5915a85ab0f7d8
Adam Johnston <adam.johnston3679@gmail.com>
2016-04-25 23:17:25 +0100

    two new helper functions for getting the first object of an object and create functions from strings

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,13 +1,31 @@
 //    capitalise : string -> string
-const capitalise = (string) => {
+const capitalise = string => {
   return string.split(' ').map(word => {
     return word.charAt(0).toUpperCase() + word.slice(1)
   }).join(' ')
 }
 
-//    hypenate : string -> string
-const hyphenate = (string) => {
+//    hyphenate : string -> string
+const hyphenate = string => {
   return string.split(' ').reduce((acc, word) => `${acc}-${word}`)
 }
 
-export { capitalise, hyphenate }
+//    first : object -> object
+const first = obj => {
+  return obj[Object.keys(obj)[0]]
+}
+
+const createFunc = (propTypes, props) => {
+  const propTypeKeys = Object.keys(propTypes)
+  const propKeys = Object.keys(props)
+
+  return propTypeKeys.reduce((acc, key, i) => {
+    (propKeys.includes(key) && propTypes[key] === 'func') ?
+      acc[key] = new Function(props[key]) :
+      acc[key] = props[key]
+
+    return acc
+  }, {})
+}
+
+export { capitalise, hyphenate, first, createFunc }
