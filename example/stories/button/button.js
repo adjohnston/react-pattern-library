a0bcd6ca3323fe22c39c8a3b52e83cb5ccad2abc
Adam Johnston <adam@getkahoot.com>
2016-05-03 13:46:10 +0100

    more examples, can remove later

--- example/stories/button/button.js
+++ example/stories/button/button.js
@@ -8,15 +8,21 @@ module.exports = {
 
     presets: {
       Default: {
-        className: '',
+        className: 'btn',
         onClick: 'alert("normal")',
         children: 'A basic button',
       },
 
       Warning: {
-        className: 'btn--warning',
+        className: 'btn btn--warning',
         onClick: 'alert("warning")',
         children: 'A warning button'
+      },
+
+      Large: {
+        className: 'btn btn--large',
+        onClick: 'console.log("large")',
+        children: 'A large button'
       }
     },
   }
---
+++ example/stories/button/readme.md
@@ -0,0 +1,5 @@
+# Button
+---
+## Modifiers
+- Warning
+- Large
