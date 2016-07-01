09a3263ff18d0fa644d12846e299aa8f7e7b53d0
Adam Johnston <adam.johnston3679@gmail.com>
2016-07-01 11:03:26 +0100

    a component for building up navigation

---
+++ app/components/Nav.js
@@ -0,0 +1,57 @@
+import React, { createClass } from 'react'
+import { Link } from 'react-router'
+
+const Nav = ({childRoutes}) => {
+  const groups = childRoutes.reduce((acc, {group, path, page}) => {
+    if (!acc[group])
+      acc[group] = []
+
+    acc[group].push({path, page})
+
+    return acc
+  }, {})
+
+  return (
+    // <ul>
+    //   {childRoutes.map(({page, path}, i) => {
+    //     return (
+    //       <li
+    //         key={i}>
+    //         <Link
+    //           to={path}>
+    //           {page}
+    //         </Link>
+    //       </li>
+    //     )
+    //   })}
+    // </ul>
+
+    <ul>
+      {Object.keys(groups).map((group, i) => {
+        return (
+          <li
+            key={i}>
+            <div>
+              {group}
+            </div>
+            <ul>
+              {groups[group].map(({path, page}, i) => {
+                return (
+                  <li
+                    key={i}>
+                    <Link
+                      to={path}>
+                      {page}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default Nav
