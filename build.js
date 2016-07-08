07a1fc26364b110c305d95bb5801163dc1f30454
Adam Johnston <adam@getkahoot.com>
2016-07-08 16:05:22 +0100

    clone styles is now part of the build step

--- build.js
+++ build.js
@@ -4,6 +4,7 @@ const args = require('minimist')(process.argv.splice(2))
 
 const compDir = args.components
 const storyDir = args.stories
+const styleFile = args.styles
 
 //    getFileNameFromPath : string -> string
 const getFileNameFromPath = () => {
@@ -26,6 +27,13 @@ const getComponent = path => {
       if (err) rej(err)
 
       res(componentsPath)
+
+const cloneFile = file => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) return err
+
+    fs.outputFile('clones/styles/cloned-styles.css', data, err => {
+      if (err) return err
     })
   })
 }
@@ -62,6 +70,10 @@ const getComponents = glob(`${compDir}**/*.js*`).then(paths => {
     }).catch(err => console.log(err))
 })
 
+const getStyles = () => {
+  cloneFile(styleFile)
+}
+
 const getSpecs = glob(`${storyDir}**/*.js`).then(paths => {
   Promise.all(paths.map(path => getSpec(path)))
     .then(specs => {
@@ -81,3 +93,5 @@ const getNotes = glob(`${storyDir}**/*.md`).then(paths => {
       `)
     })
 })
+
+getStyles()
